6a1ff9901fc1b88837c2308ce386ec296d92b708
Prashant Mahesvara <prashantmahesvara@gmail.com>
2018-07-01 10:16:44 +0200

    Removed Seed Information to make the database start as empty

--- ContosoUniversity/ContosoUniversity.Web/Migrations/201806211917157_SeedInstructors.cs
+++ ContosoUniversity/ContosoUniversity.Web/Migrations/201806211917157_SeedInstructors.cs
@@ -6,14 +6,6 @@ namespace ContosoUniversity.Web.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Niel', '', 'DeGrasse Tyson', '20180101')");
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Carl', '', 'Sagan', '20150101')");
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Mae', '', 'Jemison', '20180101')");
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Carolyn', '', 'Porco', '20180101')");
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Marie', '', 'Curie', '20180101')");
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Rosalind', '', 'Franklin', '20180101')");
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Heddy', '', 'Lamarr', '20180101')");
-            Sql("INSERT INTO Instructors (FirstName, MiddleName, LastName, HireDate) VALUES ('Ada', '', 'Lovelace', '20180101')");
         }
 
         public override void Down()
--- ContosoUniversity/ContosoUniversity.Web/Migrations/201806212108065_AddedDepartmentClass.cs
+++ ContosoUniversity/ContosoUniversity.Web/Migrations/201806212108065_AddedDepartmentClass.cs
@@ -7,13 +7,6 @@ namespace ContosoUniversity.Web.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO Departments (Name, Budget, StartDate) VALUES ('Department of Natural Science', '100,00', '20180101')");
-            Sql("INSERT INTO Departments (Name, Budget, StartDate) VALUES ('Department of Social Science', '100,00', '20180101')");
-            Sql("INSERT INTO Departments (Name, Budget, StartDate) VALUES ('Department of Human Science', '100,00', '20180101')");
-            Sql("INSERT INTO Departments (Name, Budget, StartDate) VALUES ('Department of Mechanical Science', '100,00', '20180101')");
-            Sql("INSERT INTO Departments (Name, Budget, StartDate) VALUES ('Department of Computer Science', '100,00', '20180101')");
-            Sql("INSERT INTO Departments (Name, Budget, StartDate) VALUES ('Department of Health and Nutrition', '100,00', '20180101')");
-            Sql("INSERT INTO Departments (Name, Budget, StartDate) VALUES ('Department of Justice and Law', '100,00', '20180101')");
         }
         
         public override void Down()
